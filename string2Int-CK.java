623cc8f02670577d0151e18d7d0ebfab7d1e1abe
Nitheesh mohan <nitheeshmohan@ymail.com>
2015-06-30 23:48:11 +0530

    Create string2Int-CK.java

---
+++ string2Int-CK.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class String2Int{
+    
+    public static void main(String args[]){
+        String s=new Scanner(System.in).nextLine();
+        int i =Integer.parseInt(s);
+        i=i+1;//jus to prove the string has been converted
+        System.out.println(i);
+        
+    }
+}
